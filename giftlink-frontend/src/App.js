a243c66d3e9b46e543d5b7f54a6e435baff8b022
akhigbe sadiq <akhigbesadiq@gmail.com>
2026-03-04 05:52:11 -0500

    Implement search functionality and style SearchPage.

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/search" element={<SearchPage/>} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
         </Routes>
